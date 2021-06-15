a3517b0efa78bad7cd67e5ad5721f9f0efd116c0
Jörg <joerg@lauenroth.org>
2021-06-15 12:45:24 +0200

    image gallery

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -3,6 +3,15 @@ import styled, { createGlobalStyle } from "styled-components";
 import "@fontsource/comfortaa";
 import "@fontsource/fira-code";
 import { Link } from "gatsby";
+import AltesMuseum from "../images/altes_museum.jpeg";
+import Bellevue from "../images/bellevue.jpeg";
+import Cinquantenaire from "../images/cinquantenaire.jpeg";
+import Flachau from "../images/flachau.jpeg";
+import Ghent from "../images/ghent.jpeg";
+import HumboldtForum from "../images/humboldt_forum.jpeg";
+import Kellenhusen from "../images/kellenhusen.jpeg";
+import Roma from "../images/roma.jpeg";
+import Zoo from "../images/zoo.jpeg";
 
 const GlobalStyle = createGlobalStyle`
   :root {
@@ -150,17 +159,36 @@ const MainLayout: React.FC = ({ children }) => (
         <div>
           <h3>Photo stream</h3>
           <Gallery>
-            <img src="https://scontent-frt3-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/s640x640/132090331_402509710994049_8952866939266705046_n.jpg?tp=1&_nc_ht=scontent-frt3-1.cdninstagram.com&_nc_cat=109&_nc_ohc=gU0lOQSyaAQAX-wGUqD&edm=AABBvjUAAAAA&ccb=7-4&oh=8fbc371152d9bca61aca3a11013450e2&oe=609E5AAE&_nc_sid=83d603" />
-            <img src="https://scontent-frt3-1.cdninstagram.com/v/t51.2885-15/e35/103794853_269082347740027_5344177706256895170_n.jpg?tp=1&_nc_ht=scontent-frt3-1.cdninstagram.com&_nc_cat=107&_nc_ohc=Gx_UlCuUz-sAX9N_Alz&edm=AP_V10EAAAAA&ccb=7-4&oh=cdf4abb1280573defb9bfdabc5bcf2c4&oe=60A071F7&_nc_sid=4f375e" />
-            <img src="https://scontent-frt3-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/s750x750/13398744_1745194305762525_1495246471_n.jpg?tp=1&_nc_ht=scontent-frt3-1.cdninstagram.com&_nc_cat=109&_nc_ohc=dic9LmtVT5gAX_GeK1j&edm=AP_V10EAAAAA&ccb=7-4&oh=1d59f32b5c538a016e3d281d1777d031&oe=609F469A&_nc_sid=4f375e" />
-            <img src="https://scontent-frt3-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/s640x640/31136712_1785653061457965_8338139329658355712_n.jpg?tp=1&_nc_ht=scontent-frt3-1.cdninstagram.com&_nc_cat=109&_nc_ohc=swRQH2xAoTQAX-HY6Lt&edm=AP_V10EAAAAA&ccb=7-4&oh=d04476fcc1fd489238757386a1a3d03b&oe=609FFBE5&_nc_sid=4f375e" />
-            <img src="https://scontent-frx5-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/s640x640/13423067_465821903611313_178480076_n.jpg?tp=1&_nc_ht=scontent-frx5-1.cdninstagram.com&_nc_cat=105&_nc_ohc=znD_DbIzTo4AX_uLa08&edm=AP_V10EAAAAA&ccb=7-4&oh=1666c05d4c47df9693ec0f0e89ba531d&oe=60A08CE7&_nc_sid=4f375e" />
-            <img src="https://scontent-frx5-1.cdninstagram.com/v/t51.2885-15/e35/64600113_191013988554526_6800407250889022748_n.jpg?tp=1&_nc_ht=scontent-frx5-1.cdninstagram.com&_nc_cat=110&_nc_ohc=6NOGPj0C_OkAX-xr4VH&edm=AP_V10EAAAAA&ccb=7-4&oh=ffa51af2cafae1acc4580ef8f7bd8ec0&oe=60A05A42&_nc_sid=4f375e" />
-            <img src="https://scontent-frt3-2.cdninstagram.com/v/t51.2885-15/e15/11203396_734270750011633_477080061_n.jpg?tp=1&_nc_ht=scontent-frt3-2.cdninstagram.com&_nc_cat=101&_nc_ohc=sge7R_u5MakAX9S-blC&edm=AP_V10EAAAAA&ccb=7-4&oh=c7774c60893e666679f15020bef39e9d&oe=609F2EFE&_nc_sid=4f375e" />
-            <img src="https://scontent-frt3-2.cdninstagram.com/v/t51.2885-15/fr/e15/s1080x1080/50230778_566047660562007_6265317297421567443_n.jpg?tp=1&_nc_ht=scontent-frt3-2.cdninstagram.com&_nc_cat=103&_nc_ohc=sGr6IBKponQAX_TSy2u&edm=AP_V10EAAAAA&ccb=7-4&oh=c21fa8c2a62e54ade007fd0f71305bd1&oe=60A01C4F&_nc_sid=4f375e" />
-            <img src="https://scontent-frx5-1.cdninstagram.com/v/t51.2885-15/e35/84705540_194664208560143_5603665843698671134_n.jpg?tp=1&_nc_ht=scontent-frx5-1.cdninstagram.com&_nc_cat=111&_nc_ohc=_EPjgciJwtMAX_WNcJ1&edm=AP_V10EAAAAA&ccb=7-4&oh=49de395e7d6ca971ebddc5c6f02b34a1&oe=60A03CF7&_nc_sid=4f375e" />
-            <img src="https://scontent-frt3-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/s750x750/83103497_121441269229394_161422819949146411_n.jpg?tp=1&_nc_ht=scontent-frt3-1.cdninstagram.com&_nc_cat=102&_nc_ohc=l-0wYmTkIIgAX-lT5aZ&tn=69tHmlsoDHvWPaT0&edm=AP_V10EAAAAA&ccb=7-4&oh=82d325164a961647b6162958421c26c7&oe=60A01DB3&_nc_sid=4f375e" />
-            <img src="https://scontent-frt3-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/s750x750/66071288_127663805169304_835903907610460472_n.jpg?tp=1&_nc_ht=scontent-frt3-1.cdninstagram.com&_nc_cat=104&_nc_ohc=1jU3TL6N1OkAX_Kmzt3&edm=AABBvjUAAAAA&ccb=7-4&oh=2e1eac12e722e2bb03147593aa537d8d&oe=60A0CDBB&_nc_sid=83d603" />
+            <a href="https://www.instagram.com/p/CJHCgoSgchw/">
+              <img src={AltesMuseum} alt="Altes Museum, Berlin" />
+            </a>
+            <a href="https://www.instagram.com/p/BHCjWn1AtIy/">
+              <img
+                src={Cinquantenaire}
+                alt="Parc du Cinquantenaire, Brussels"
+              />
+            </a>
+            <a href="https://www.instagram.com/p/BG9-c1UuzOG/">
+              <img src={Ghent} alt="Ghent, Belgium" />
+            </a>
+            <a href="https://www.instagram.com/p/Bibwnc9lIqo/">
+              <img src={Zoo} alt="Zoo, Berlin" />
+            </a>
+            <a href="https://www.instagram.com/p/B8ORsZOC_X5/">
+              <img src={Flachau} alt="Reitdorf, Austria" />
+            </a>
+            <a href="https://www.instagram.com/p/CJHCYGrgU-m/">
+              <img src={HumboldtForum} alt="Humboldt Forum, Berlin" />
+            </a>
+            <a href="https://www.instagram.com/p/CC58G_zJFWL/">
+              <img src={Kellenhusen} alt="Kellenhusen" />
+            </a>
+            <a href="https://www.instagram.com/p/BVQApT4DTL3/">
+              <img src={Roma} alt="Collosseum, Rome" />
+            </a>
+            <a href="https://www.instagram.com/p/BJhlrHyA6UP/">
+              <img src={Bellevue} alt="Bellevue, Berlin" />
+            </a>
           </Gallery>
         </div>
       </section>
@@ -424,7 +452,7 @@ const MainFooter = styled.footer`
 
     &:first-child {
       display: grid;
-      grid-template-columns: 3fr 6fr;
+      grid-template-columns: auto 700px;
 
       ul {
         list-style: none;
